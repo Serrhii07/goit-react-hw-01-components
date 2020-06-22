96ce8dff8b62f35567a0949a9ab29c9778dadd1c
Serrhii07 <serge070989@gmail.com>
2020-06-22 14:30:34 +0300

    Updates

---
+++ precache-manifest.e0f31a6970cd00b705509614b7ae2ab7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88cef52970ff662d411eb1de017e2a2",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "c850006168ee8d793745",
+    "url": "/goit-react-hw-01-components/static/css/2.4b502e0b.chunk.css"
+  },
+  {
+    "revision": "ee5168dc9417d42d8b44",
+    "url": "/goit-react-hw-01-components/static/css/main.25d867fc.chunk.css"
+  },
+  {
+    "revision": "c850006168ee8d793745",
+    "url": "/goit-react-hw-01-components/static/js/2.a0ffe722.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.a0ffe722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee5168dc9417d42d8b44",
+    "url": "/goit-react-hw-01-components/static/js/main.c46126f1.chunk.js"
+  },
+  {
+    "revision": "cf184cad0926235fce4a",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.f8c9733c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-01-components/precache-manifest.991b953f3a5ce93371c5f14f7e64c240.js"
+  "/goit-react-hw-01-components/precache-manifest.e0f31a6970cd00b705509614b7ae2ab7.js"
 );
 
 self.addEventListener('message', (event) => {
